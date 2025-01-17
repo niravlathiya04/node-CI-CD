b5fd1459c3236667d4463b4664acab9ff72dd6c3
niravlathiya04 <niravlathiya390@gmail.com>
2025-01-17 14:58:49 +0530

    Enhance error logging in app.ts by adding a structured error message during server startup. This change improves debugging capabilities and provides clearer visibility into potential issues during development.

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ app.use('/api', router);
 // Start server
 app.listen(port, '0.0.0.0', () => {
   console.log("This is my first project of Node CI/CD demo");
+  console.error(JSON.stringify({level: "error", message:"Oops ! here is first Error"}))
   console.log(JSON.stringify({ level: "info", message: "Server started successfully" }));
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
